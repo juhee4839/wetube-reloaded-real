8c15500d171373b2213dc635e5370c476acd97bc
juhee <79627080+juhee4839@users.noreply.github.com>
2021-08-06 11:11:24 +0900

    Re

--- src/db.js
+++ src/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", { useNewUrlParser: true, useUnifiedTopology: true } )
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify:false, 
+});
 //mongodb://127.0.0.1:27017/
 
 const db = mongoose.connection;
